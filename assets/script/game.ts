bb7e57d3d355caee6f7745d5e28e732ddcbd75f1
iloveuav <949177603@qq.com>
2021-06-14 20:24:40 +0800

    新增代码自动生成地图碰撞box

--- assets/script/game.ts
+++ assets/script/game.ts
@@ -5,55 +5,86 @@
 // Learn life-cycle callbacks:
 //  - https://docs.cocos.com/creator/manual/en/scripting/life-cycle-callbacks.html
 
-const { ccclass, property } = cc._decorator;
+const { ccclass, property } = cc._decorator
 
 @ccclass
 export default class NewClass extends cc.Component {
-  @property(cc.Label)
-  label: cc.Label = null;
+	@property(cc.Label)
+	label: cc.Label = null
 
-  @property
-  text: string = "hello";
+	@property
+	text: string = 'hello'
 
-  @property(cc.Node)
-  mapNode: cc.Node = null;
+	@property(cc.Node)
+	mapNode: cc.Node = null
+	@property(cc.Node)
+	tiledMap: cc.TiledMap = null
 
-  // LIFE-CYCLE CALLBACKS:
+	// LIFE-CYCLE CALLBACKS:
 
-  // onLoad () {}
+	onLoad() {
+		let p = cc.director.getPhysicsManager()
+		p.enabled = true //启用物理组件
+		// cc.director.getPhysicsManager().gravity = cc.v2(0，0);//无重力
+		p.gravity = cc.v2(0, -20) //每秒降落20个世界单位
+		cc.director.getPhysicsManager().debugDrawFlags = 3
+		// this.initMapNode(this.mapNode);
+	}
 
-  start() {}
+	initMapNode(mapNode: cc.Node) {
+		let tiledMap = mapNode.getComponent(cc.TiledMap)
+		let tiledSize = tiledMap.getTileSize()
+		let layer = tiledMap.getLayer('wall')
+		console.log('layer: ', layer)
+		let layerSize = layer.getLayerSize()
+		for (let i = 0; i < layerSize.width; i++) {
+			for (let j = 0; j < layerSize.height; j++) {
+				let tiled = layer.getTiledTileAt(i, j, true)
+				console.log('tiled: ', tiled)
+				if (tiled.gid != 0) {
+					tiled.node.group = 'wall'
 
-  onLoad() {
-    cc.director.getPhysicsManager().enabled = true; //启用物理组件
-    // cc.director.getPhysicsManager().gravity = cc.v2(0，0);//无重力
-    cc.director.getPhysicsManager().gravity = cc.v2(0, -20); //每秒降落20个世界单位
-    // cc.director.getPhysicsManager().debugDrawFlags = 6;
-    // this.initMapNode(this.mapNode);
-  }
+					let body = tiled.node.addComponent(cc.RigidBody)
+					body.type = cc.RigidBodyType.Static
+					let collider = tiled.node.addComponent(
+						cc.PhysicsBoxCollider
+					)
+					collider.offset = cc.v2(
+						tiledSize.width / 2,
+						tiledSize.height / 2
+					)
+					collider.apply()
+				}
+			}
+		}
+	}
 
-  initMapNode(mapNode: cc.Node) {
-    let tiledMap = mapNode.getComponent(cc.TiledMap);
-    let tiledSize = tiledMap.getTileSize();
-    let layer = tiledMap.getLayer("wall");
-    console.log("layer: ", layer);
-    let layerSize = layer.getLayerSize();
-    for (let i = 0; i < layerSize.width; i++) {
-      for (let j = 0; j < layerSize.height; j++) {
-        let tiled = layer.getTiledTileAt(i, j, true);
-        console.log("tiled: ", tiled);
-        if (tiled.gid != 0) {
-          tiled.node.group = "wall";
+	start() {
+        let tiledMap = this.mapNode.getComponent(cc.TiledMap)
+		let tiledSize = tiledMap.getTileSize()
 
-          let body = tiled.node.addComponent(cc.RigidBody);
-          body.type = cc.RigidBodyType.Static;
-          let collider = tiled.node.addComponent(cc.PhysicsBoxCollider);
-          collider.offset = cc.v2(tiledSize.width / 2, tiledSize.height / 2);
-          collider.apply();
-        }
-      }
-    }
-  }
 
-  // update (dt) {}
+        // let tiledSize = this.tileMap.getTileSize();//拿到一个小块的尺寸
+
+
+		// let layer = this.tileMap.getLayer('wall')
+		let layer = tiledMap.getLayer('wall')
+		let layerSize = layer.getLayerSize() //不是拿像素 是拿块尺寸
+		for (let i = 0; i < layerSize.width; i++) {
+			for (let j = 0; j < layerSize.height; j++) {
+				let tiled = layer.getTiledTileAt(i, j, true);
+                if(tiled.gid!=0){
+                    tiled.node.group = 'wall';
+                    let body = tiled.node.addComponent(cc.RigidBody);
+                    body.type = cc.RigidBodyType.Static;
+                    let collider = tiled.node.addComponent(cc.PhysicsBoxCollider)
+                    collider.offset = cc.v2(tiledSize.width/2,tiledSize.height/2);//中心点
+                    collider.size = tiledSize;
+                    collider.apply();//没有不会生效
+                }
+			}
+		}
+	}
+
+	// update (dt) {}
 }
